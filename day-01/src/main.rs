70fdfb021f17ba712701b85fa6ab12f18f1ab89a
Mark Sherry <mdsherry@gmail.com>
2020-12-02 09:51:47 -0500

    Break the outer loop

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -6,12 +6,12 @@ static INPUT: &str = include_str!("input.txt");
 
 fn main() {
     let numbers: BTreeSet<_> = parse_lines::<u32>(INPUT).collect();
-    for &number in &numbers {
+    'outer: for &number in &numbers {
         for &other_number in numbers.range((Excluded(number), Included(2020 - number))) {
             let sum = number + other_number;
             if numbers.contains(&(2020 - sum)) {
                 println!("{} * {} * {} = {}", number, other_number, sum, number * other_number * (2020 - sum));
-                break;
+                break 'outer;
             }
         }
     }    
